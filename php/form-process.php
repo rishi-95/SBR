43efa15fea1666df26ba98b07d96e8753aa32424
rishi-95 <rishisiliveri243@gmail.com>
2021-09-03 22:53:42 +0530

    test mail

--- php/form-process.php
+++ php/form-process.php
@@ -38,7 +38,7 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "info@sbrinfraprojects.com";
+$EmailTo = "rishisiliveri243@gmail.com";
 $Subject = "New Message Received";
 
 // prepare email body text
